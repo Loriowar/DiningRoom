aeb24b5754d7d90e023bae0bb5839d4671f59625
vampirka <energovampirka@gmail.com>
2014-03-14 14:22:37 +0400

    Scaffolds and migrations   - add migration for dish_mixtures # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # Committer: Vampirka <vampirka@Vampirka> # # On branch master # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	modified:   config/database.yml #	new file:   db/migrate/20140301114355_create_foodstuff.rb #	new file:   db/migrate/20140301120133_create_supplies.rb #	new file:   db/migrate/20140301120152_create_providers.rb #	new file:   db/migrate/20140301120211_create_cashbox.rb #	new file:   db/migrate/20140301120228_create_employees.rb #	new file:   db/migrate/20140301120259_create_appointments.rb #	new file:   db/migrate/20140301120315_create_menu.rb #	new file:   db/migrate/20140301120330_create_dishes.rb #	new file:   db/migrate/20140301120400_create_production_plans.rb #	new file:   db/migrate/20140301120420_create_clients.rb #	new file:   db/migrate/20140301120438_create_complaints.rb #	new file:   db/schema.rb #

---
+++ db/migrate/20140314101659_creat_dish_mixtures.rb
@@ -0,0 +1,19 @@
+class CreatDishMixtures < ActiveRecord::Migration
+  def up
+    execute <<-SQL
+      CREATE TABLE dish_mixtures (
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        dish_id INT(11),
+        foodstuff_id INT(11),
+        PRIMARY KEY (id)
+
+      ) ENGINE=InnoDB CHARACTER SET=utf8;
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP TABLE dish_mixtures;
+    SQL
+  end
+end
